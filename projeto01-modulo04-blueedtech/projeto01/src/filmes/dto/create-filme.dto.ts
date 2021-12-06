12ce58d1b932f9ec66e6b1884b109a12e5e3f18a
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-12-06 09:50:19 -0300

    ok

--- projeto01-modulo04-blueedtech/projeto01/src/filmes/dto/create-filme.dto.ts
+++ projeto01-modulo04-blueedtech/projeto01/src/filmes/dto/create-filme.dto.ts
@@ -1,1 +1,24 @@
-export class CreateFilmeDto {}
+import { IsNotEmpty, IsInt, IsString, isString, isDate, IsDate, IsNumber } from 'class-validator';
+export class CreateFilmeDto {
+
+  @IsString()
+  @IsNotEmpty()
+  nome: string
+
+  @IsString()
+  imagem: string
+
+  @IsDate()
+  @IsNotEmpty()
+  data_lancamento: number
+
+  @IsNotEmpty()
+  @IsNumber()
+  tempo_duracao: number
+
+  
+
+
+
+  
+}
