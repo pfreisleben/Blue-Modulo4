937e6eb1f2aea5fd35de3dcf5c298036b77b222d
pfreisleben <pedrofreislebeen@gmail.com>
2021-12-06 19:30:00 -0300

    ok

--- projeto01/src/generos/generos.module.ts
+++ projeto01/src/generos/generos.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GenerosService } from './generos.service';
 import { GenerosController } from './generos.controller';
+import { PrismaClient } from '.prisma/client';
 
 @Module({
   controllers: [GenerosController],
-  providers: [GenerosService]
+  providers: [GenerosService, PrismaClient],
 })
 export class GenerosModule {}
